912a5e017a8dfcc662aa7ea1de240d9d357e47c9
jcvictor18 <joao.itapox@gmail.com>
2025-04-21 21:59:48 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,8 +4,4 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"mensagem": "Bem-vindo ao app FastAPI com CI/CD!"}
-
-@app.get("/saudacao/{nome}")
-def saudacao(nome: str):
-    return {"mensagem": f"Olá, {nome}!"}
+    return {"message": "Hello, World!"}
