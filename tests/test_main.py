11e314cd0136828a7a596a821e20d5659f4e14a5
João Victor <joao.itapox@gmail.com>
2025-04-21 22:31:46 -0300

    Primeiro commit do projeto

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,12 +1,17 @@
 import sys
 import os
+from fastapi.testclient import TestClient
 
 # Adicionar o diretório 'app' ao caminho de busca do Python
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'app')))
 
+<<<<<<< HEAD
 import pytest
 from fastapi.testclient import TestClient
 from app.main import app  # Certifique-se de que o caminho está correto
+=======
+from main import app
+>>>>>>> ba1d5c5 (Configuração do workflow do GitHub Actions com FastAPI e testes)
 
 client = TestClient(app)
 
