461a31e34f82303e3d08e1ebb0faf7bd9c2830aa
João Victor <joao.itapox@gmail.com>
2025-04-21 21:44:47 -0300

    Corrigindo importações e configurando o CI/CD

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,11 @@
+import sys
+import os
+
+# Adicionar o diretório 'app' ao caminho de busca do Python
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'app')))
+
+from main import app
 from fastapi.testclient import TestClient
-from app.main import app
 
 client = TestClient(app)
 
